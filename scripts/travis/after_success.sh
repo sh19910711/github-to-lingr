40981532823ae3d3c965fa082ac5a534069f3a7d
Hiroyuki Sano <sh19910711@gmail.com>
2014-03-13 19:22:19 +0900

    Add scripts/travis/after_success.sh

---
+++ scripts/travis/after_success.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "${TRAVIS_BRANCH}" == "heroku/production" ] || [ "${TRAVIS_BRANCH}" == "heroku/development" ]; then
+  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  if [ "${TRAVIS_BRANCH}" == "heroku/production" ]; then
+    git remote add heroku git@heroku.com:${HEROKU_APP_ID_PRODUCTION}.git
+  fi
+  if [ "${TRAVIS_BRANCH}" == "heroku/development" ]; then
+    git remote add heroku git@heroku.com:${HEROKU_APP_ID_DEVELOPMENT}.git
+  fi
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  heroku config:set GITHUB_TO_LINGR_VERSION=`git rev-parse ${TRAVIS_BRANCH}`
+fi
